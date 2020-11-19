985311bed68067e62178d08e74e5ab4d8b2d0940
iamebube-ekpe <victorekpe11@gmail.com>
2020-11-19 05:55:54 +0100

    EVEL -> BS Alg

---
+++ bubble_sort.py
@@ -0,0 +1,14 @@
+def bubbleSort(uList):
+    sizeOfList = len(uList) - 1
+
+    for num in range(sizeOfList):
+        for index in range(sizeOfList-num):
+            if uList[index] > uList[index+1]:
+                uList[index],uList[index+1] = uList[index+1], uList[index]
+
+    return uList
+
+userList = input("Enter a list of values: ").split()
+userList = [int(x) for x in userList]
+print(f"You entered: {userList}")
+print(f"Bubble Sorted List: {bubbleSort(userList)}")
