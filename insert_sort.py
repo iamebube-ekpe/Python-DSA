c9c0783824c53e89cd170d5203165df4d1cc2f78
iamebube-ekpe <victorekpe11@gmail.com>
2020-11-22 04:40:24 +0100

    Commented out the return statement

--- insert_sort.py
+++ insert_sort.py
@@ -1,5 +1,5 @@
 def insertSort(uList):
-    sortedResult = []
+    # sortedResult = []
     for index in range(1, len(uList)):
         keyElement = uList[index]
         element = index - 1
@@ -8,9 +8,9 @@ def insertSort(uList):
             element -= 1
 
         uList[element+1] = keyElement
-    sortedResult.append(uList)
-    return sortedResult # returns a list of list -> fix
+    # sortedResult.append(uList)
+    # return sortedResult # returns a list of list -> fix
 
 mylist = [90,2,40,1,0,20]
-print(insertSort(mylist))
-# print(mylist)
+insertSort(mylist)
+print(mylist)
